f7a5c518d80be7cd601c8fe2c7af39c9a6310a0d
Gruppen <kolodiva@mail.ru>
2019-05-23 17:13:38 +0300

    vittorio

---
+++ webpack.config.prod.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+
+
+module.exports = {
+  // devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },      
+
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },  
+  plugins: [
+    
+    // new webpack.HotModuleReplacementPlugin()
+
+  ],  
+  devServer: {
+    contentBase: './dist',
+    hot: false,
+    port: 3000
+  }
+};
